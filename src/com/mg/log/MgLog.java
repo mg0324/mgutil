ecaf085eaa66988db2392ad582f654411fe8cf09
mengwang <1092017732@qq.com>
2016-05-10 13:24:05 +0800

    初始化

---
+++ src/com/mg/log/MgLog.java
@@ -0,0 +1,11 @@
+package com.mg.log;
+
+import org.apache.log4j.Logger;
+/**
+ * mgfinal日志器
+ * @author mg
+ * @date 2016-5-10
+ */
+public class MgLog {
+	public static Logger log = Logger.getLogger(MgLog.class);
+}
